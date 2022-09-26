80e51f3697838e6dd22125660fc71c73d2529f7a
Tarek Bernoussi <tbernoussi@stpbb.org>
2022-09-26 17:24:11 +0200

    ajout models

--- lesTaches/admin.py
+++ lesTaches/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from lesTaches.models import Task, User
+
+
 # Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'creation_date', 'schedule_date', 'due_date', 'owner')
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email',)
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(User, UserAdmin)
--- lesTaches/models.py
+++ lesTaches/models.py
@@ -1,3 +1,30 @@
+import datetime as DT
+from django.utils import timezone
+
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+    username = None
+    email = models.EmailField('email adress', unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.email
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    creation_date = models.DateField(auto_now=True)
+    closed = models.BooleanField(default=False)
+    due_date = models.DateField(default=timezone.now)
+    schedule_date = models.DateField(default=(timezone.now() + DT.timedelta(days=7)))
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return self.name
