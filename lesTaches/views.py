45d40cbbca6270438619e54bace4fd015a1a65b3
Tarek Bernoussi <tbernoussi@stpbb.org>
2022-09-26 17:49:56 +0200

    add listing

--- lesTaches/views.py
+++ lesTaches/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from lesTaches.models import Task
+
 
 # Create your views here.
-def home(request):
-    return HttpResponse('Bonjour à tous')
+def task_listing(request):
+    objects = Task.objects.all().order_by("due_date")
+    return render(request, template_name="liste.html", context={'taches':objects})
